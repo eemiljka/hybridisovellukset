92b384f4ff8de89b85f40f8150ae4384cc5c5466
eemiljka <eemiljka@metropolia.fi>
2024-01-18 14:24:04 +0200

    env file plus requests.http modified

--- media-api/src/api/models/mediaModel.ts
+++ media-api/src/api/models/mediaModel.ts
@@ -96,10 +96,10 @@ const fetchMediaById = async (id: number): Promise<MediaItem | null> => {
 const postMedia = async (
   media: Omit<MediaItem, 'media_id' | 'created_at'>
 ): Promise<MediaItem | null> => {
-  const {user_id, filename, filesize, media_type, title, description, app_id} =
+  const {user_id, filename, filesize, media_type, title, description} =
     media;
   const sql = `INSERT INTO MediaItems (user_id, filename, filesize, media_type, title, description, app_id)
-               VALUES (?, ?, ?, ?, ?, ?, ?)`;
+               VALUES (?, ?, ?, ?, ?, ?)`;
   const params = [
     user_id,
     filename,
@@ -107,7 +107,6 @@ const postMedia = async (
     media_type,
     title,
     description,
-    app_id,
   ];
   try {
     const result = await promisePool.execute<ResultSetHeader>(sql, params);
