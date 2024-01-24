cf733d0c7adf29a50d2f5de5caf3748254f539c6
eemiljka <eemiljka@metropolia.fi>
2024-01-24 14:01:43 +0200

    schemas initialize and testing

--- graphql-server/src/api/resolvers/mediaResolver.ts
+++ graphql-server/src/api/resolvers/mediaResolver.ts
@@ -1,9 +1,14 @@
-import {fetchAllMedia} from '../models/mediaModel';
+import {fetchAllMedia, fetchMediaById} from '../models/mediaModel';
 
 export default {
-    Query: {
-        mediaItems: async () => {
-            return await fetchAllMedia();
-        },
+  Query: {
+    mediaItems: async () => {
+      return await fetchAllMedia();
     },
+    mediaItem: async (_parent: undefined, args: {media_id: string}) => {
+      console.log(args);
+      const id = Number(args.media_id);
+      return await fetchMediaById(id);
+    },
+  },
 };
